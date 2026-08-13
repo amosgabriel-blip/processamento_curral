d3221277f4c9bdd63895f04f99578f219b36f7dd
amosgabriel-blip <amosgabrielfilho@gmail.com>
2026-08-13 16:53:30 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'processamento-gol-v1';
+const CACHE_NAME = 'processamento-gol-v4';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
