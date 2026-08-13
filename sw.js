487461502177af781f2cf2d0c665d6520de1688a
amosgabriel-blip <amosgabrielfilho@gmail.com>
2026-08-13 18:25:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'processamento-gol-v5';
+const CACHE_NAME = 'processamento-gol-v6';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
