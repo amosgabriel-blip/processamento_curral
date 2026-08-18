440d81713eaf35b72ea3e345baba7a0378af25c2
amosgabriel-blip <amosgabrielfilho@gmail.com>
2026-08-18 06:55:08 -0300

    Update CACHE_NAME from v7 to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'processamento-gol-v7';
+const CACHE_NAME = 'processamento-gol-v9';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
