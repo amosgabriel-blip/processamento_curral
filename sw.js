5fd997b764c34549bfb58b042b73e285203c053b
amosgabriel-blip <amosgabrielfilho@gmail.com>
2026-08-18 14:19:05 -0300

    Update CACHE_NAME to processamento-gol-v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'processamento-gol-v9';
+const CACHE_NAME = 'processamento-gol-v10';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
