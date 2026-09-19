e14bce60eb07a933d073b105fb71cfe90a04e754
amosgabriel-blip <amosgabrielfilho@gmail.com>
2026-09-19 15:53:11 -0300

    Update CACHE_NAME to version 18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'processamento-gol-v15';
+const CACHE_NAME = 'processamento-gol-v18';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
